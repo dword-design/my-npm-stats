b894f07f6f77fd55b86081d0577c5765e59e1c34
Sebastian Landwehr <info@dword-design.de>
2021-03-15 01:53:06 +0100

    fix: foo

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -23,10 +23,10 @@ export default tester(
       ).toMatchImageSnapshot(this)
       const form = await this.page.waitForSelector('form')
       await form.evaluate(el => el.submit())
-      await delay(100)
+      await delay(200)
       expect(
         await this.page.screenshot({ fullPage: true })
-      ).toMatchImageSnapshot(this, { dumpDiffToConsole: true })
+      ).toMatchImageSnapshot(this)
       const table = await this.page.waitForSelector('table')
       const visibleRowCount = 15
       const rows = await table.$$('tbody tr')
