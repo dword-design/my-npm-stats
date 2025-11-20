7ab64dd3db15ad5607010757e8b11768a064aeb7
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-11-20 09:16:21 +0100

    fix: foo

---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import config from '@dword-design/eslint-config';
+import { globalIgnores } from 'eslint/config';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default await withNuxt(
+  globalIgnores(['eslint.config.ts', 'eslint.lint-staged.config.ts', 'ecosystem.json']),
+  config,
+  {
+    files: ['**/pages/**/*.{vue,ts}', 'server/api/**/*.ts'],
+    rules: {
+      'unicorn/filename-case': 'off',
+    },
+  },
+).toConfigs();
